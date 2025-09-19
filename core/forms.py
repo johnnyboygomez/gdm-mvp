26ae305302ef1c901ee5997f450d3a282cce5eb4
John Dowling <john@dowling.ca>
2025-09-19 07:16:12 -0400

    Fixed form.py so can change passwords

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,7 @@
 # core/forms.py
-
 from django import forms
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.utils.translation import gettext_lazy as _
 from .models import CustomUser
 
 class CustomUserCreationForm(forms.ModelForm):
@@ -11,11 +11,11 @@ class CustomUserCreationForm(forms.ModelForm):
     """
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
-
+    
     class Meta:
         model = CustomUser
         fields = ('email', 'first_name', 'last_name')
-
+    
     def clean_password2(self):
         # Check that the two password entries match
         password1 = self.cleaned_data.get("password1")
@@ -23,7 +23,7 @@ class CustomUserCreationForm(forms.ModelForm):
         if password1 and password2 and password1 != password2:
             raise forms.ValidationError("Passwords don't match")
         return password2
-
+    
     def save(self, commit=True):
         # Save the provided password in hashed format
         user = super().save(commit=False)
@@ -38,9 +38,15 @@ class CustomUserChangeForm(forms.ModelForm):
     the user, but replaces the password field with admin's
     password hash display field.
     """
-    password = ReadOnlyPasswordHashField()
-
+    password = ReadOnlyPasswordHashField(
+        label=_("Password"),
+        help_text=_(
+            'Raw passwords are not stored, so there is no way to see this '
+            'user\'s password, but you can change the password using '
+            '<a href="../password/">this form</a>.'
+        ),
+    )
+    
     class Meta:
         model = CustomUser
         fields = ('email', 'password', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser')
-
