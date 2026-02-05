925972dae0828ebc005c5cece6de3ae524664021
John Dowling <john@dowling.ca>
2026-02-05 05:37:06 -0500

    changed notifications address to partnersteprimuhc@gmail.com

--- gdm/settings.py
+++ gdm/settings.py
@@ -36,10 +36,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'rimuhc.john@gmail.com'
+EMAIL_HOST_USER = 'partnersteprimuhc@gmail.com'
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'rimuhc.john@gmail.com'   # Or another sender address if you prefer
+DEFAULT_FROM_EMAIL = 'partnersteprimuhc@gmail.com'   # Or another sender address if you prefer
 
 GOAL_NOTIFICATION_CC = [
     "john.dowling@rimuhc.ca",
